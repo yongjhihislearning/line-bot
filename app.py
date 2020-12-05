047ab76244636f9be6a4bcfdb4273719d62900e7
Yongjhih Lin <yongjhih85@gmail.com>
2020-12-05 22:06:00 +0800

    Update

--- app.py
+++ app.py
@@ -41,16 +41,16 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    reply = '很抱歉，你說什麼'
+    r = '很抱歉，你說什麼'
 
     if msg == 'hi':
-        reply = 'hi'
+        r = 'hi'
     elif msg == '你吃飯了嗎':
-        reply = '還沒'
+        r = '還沒'
 
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=s))
+        TextSendMessage(text=r))
 
 
 if __name__ == "__main__":
