b1e488fb64b9614824ad068df298d7f591662214
Yongjhih Lin <yongjhih85@gmail.com>
2020-12-05 22:09:29 +0800

    Update

--- app.py
+++ app.py
@@ -43,10 +43,14 @@ def handle_message(event):
     msg = event.message.text
     r = '很抱歉，你說什麼'
 
-    if msg == 'hi':
-        r = 'hi'
+    if msg in ['hi', 'Hi']:
+        r = '嗨'
     elif msg == '你吃飯了嗎':
         r = '還沒'
+    elif msg == '你是誰':
+        r = '我是機器人'
+    elif '訂位' in msg:
+        r = '您想訂位，是嗎?'
 
     line_bot_api.reply_message(
         event.reply_token,
